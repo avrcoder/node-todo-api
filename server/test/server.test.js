fbf16f182d91a07a9fdd7f54cca3873d6a7a60d0
ShreeVishwa <reddyvishwatej176@gmail.com>
2017-10-25 20:36:26 -0400

    Add GET /todos tests

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,17 @@ const request = require('supertest');
 var {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+	text:'First test todo'
+},{
+	text:'Second test todo'
+}];
+
 //lets run some code before each test case
 beforeEach((done) => {
-	Todo.remove({}).then(() => done()); //deletes all todos
+	Todo.remove({}).then(() => {
+		Todo.insertMany(todos);
+	}).then(() => done()); //deletes all todos
 });
 
 describe('POST /todos', () => {
@@ -20,7 +28,7 @@ describe('POST /todos', () => {
 				return done(err);
 			}
 
-			Todo.find().then((todos) => {
+			Todo.find({text}).then((todos) => {
 				expect(todos.length).toBe(1);
 				expect(todos[0].text).tobe(text);
 				done();
@@ -38,9 +46,17 @@ describe('POST /todos', () => {
 			}
 
 			Todo.fine().then((todos) => {
-				expect(todos.length).toBe(0);
+				expect(todos.length).toBe(2);
 				done();
 			}).catch((e) => done(e));
 		});
+	});
+});
+
+describe('GET /todos', () => {
+	it('Should get all todos', (done) => {
+		request(app).get('/todos').expect(200).expect((res) => {
+			expect(res.body.todos.length).toBe(2);
+		}).end(done);
 	});
 });
